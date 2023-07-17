f1ef3967cb953f8c8595724dea5cd956cede7a47
ramiel <ramielwan48@gmail.com>
2023-07-17 17:37:21 +0300

    feat: create repro

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 export default function App() {
+  const progress = useSharedValue(0.5);
+
+  const toggle = () => {
+    progress.value = withTiming(progress.value === 0.5 ? 1 : 0.5);
+  };
+
+  const rStyle = useAnimatedStyle(
+    () => ({
+      width: 100,
+      aspectRatio: progress.value,
+      backgroundColor: "red",
+    }),
+    []
+  );
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Animated.View style={rStyle} />
+      <Button title="toggle" onPress={toggle} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
